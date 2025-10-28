7c31ad715611cbfeadfd8de957a5e8519450398b
ArthurDuval <arthurduval02@gmail.com>
2025-10-28 14:26:28 +0100

    add: enable ly + rm recursive git clone

--- install.sh
+++ install.sh
@@ -15,19 +15,17 @@ cd ..
 rm -rf yay
 
 yay -S	xorg i3-wm i3lock polybar rofi kitty neovim firefox zsh xclip ripgrep \
-		unzip polkit ly discord spotify tree feh flameshot picom
+		unzip polkit ly discord spotify tree feh flameshot picom openssh
+
+sudo systemctl enable ly
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-md -p ~/Pictures/wallpaper
-md ~/.config/kitty
-md ~/.config/nvim
-md ~/.config/polybar
-md ~/.config/rofi
-
-git clone git@github.com:ArthurDuval/dotfiles.git
-
-cd dotfiles
+mkdir -p ~/Pictures/wallpaper
+mkdir ~/.config/kitty
+mkdir ~/.config/nvim
+mkdir ~/.config/polybar
+mkdir ~/.config/rofi
 
 cp i3/* ~/.config/i3
 cp kitty/* ~/.config/kitty
